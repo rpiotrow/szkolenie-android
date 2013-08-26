9957e54604d5d8d560bfd17133a5b58e418995d6
Rafał Piotrowski <rafal.piotrowski@unity.pl>
2013-08-26 16:00:44 +0200

    add button click handler

--- src/com/example/shoppinglist/ShoppingListActivity.java
+++ src/com/example/shoppinglist/ShoppingListActivity.java
@@ -3,20 +3,35 @@ package com.example.shoppinglist;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class ShoppingListActivity extends Activity {
+	
+	private Button addButton;
+	private TextView productNameView;
+	private EditText productNameInput;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
+		
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_shopping_list);
-	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.shopping_list, menu);
-		return true;
+		
+		addButton = (Button) findViewById(R.id.add_button);
+		productNameView = (TextView) findViewById(R.id.product_name_view);
+		productNameInput = (EditText) findViewById(R.id.product_name_input);
+		
+		addButton.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				productNameView.setText(productNameInput.getText());
+			}
+			
+		});
 	}
 
 }
